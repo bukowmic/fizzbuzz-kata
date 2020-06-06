fa8873dbf60192ed9cd007a1280554d74beb05b5
bukowmic <bukowmic@gmail.com>
2020-06-07 00:07:59 +0200

    Zadanie 1. Funkcja zwracająca reprezentację liczby w grze.

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -2,4 +2,14 @@ package tdd.fizzbuzz;
 
 public class FizzBuzz {
 
+    public String numberRepresentation(int number) {
+        String representation = "";
+        if (number % 3 == 0) {
+            representation += "Fizz";
+        }
+        if (number % 5 == 0) {
+            representation += "Buzz";
+        }
+        return representation.isEmpty() ? String.valueOf(number) : representation;
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,13 +1,43 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FizzBuzzTest {
+    private FizzBuzz fizzBuzz;
+
+    @BeforeEach
+    void setUp() {
+        fizzBuzz = new FizzBuzz();
+    }
 
     @Test
-    public void startHere() {
-        assertThat(4).isEqualTo(5);
+    public void number_representation_of_one_is_one() {
+        String representation = fizzBuzz.numberRepresentation(1);
+
+        assertThat(representation).isEqualTo("1");
+    }
+
+    @Test
+    void number_representation_of_three_is_Fizz() {
+        String representation = fizzBuzz.numberRepresentation(3);
+
+        assertThat(representation).isEqualTo("Fizz");
+    }
+
+    @Test
+    void number_representation_of_five_is_Buzz() {
+        String representation = fizzBuzz.numberRepresentation(5);
+
+        assertThat(representation).isEqualTo("Buzz");
+    }
+
+    @Test
+    void number_representation_of_fifteen_is_FizzBuzz() {
+        String representation = fizzBuzz.numberRepresentation(15);
+
+        assertThat(representation).isEqualTo("FizzBuzz");
     }
 }
